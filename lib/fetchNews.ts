f0d2923fbe0066fc5cf86cb9b739283282a3ec44
vberkoz <vberkoz@gmail.com>
2022-12-09 19:09:38 +0200

    Building the Fetch News Function

---
+++ lib/fetchNews.ts
@@ -0,0 +1,74 @@
+import { gql } from "graphql-request";
+import sortNewsByImage from "./sortNewsByImage";
+
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  const GET_QUERY = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        countries: "us"
+        sort: "published_desc"
+        keywords: $keywords
+      ) {
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+        data {
+          author
+          category
+          country
+          description
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+      }
+    }
+  `;
+
+  const res = await fetch(
+    "https://campobelo.stepzen.net/api/looping-liger/__graphql",
+    {
+      method: "POST",
+      cache: isDynamic ? "no-cache" : "default",
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 60 },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query: GET_QUERY,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  );
+
+  console.log("LOADING NEW DATA FROM API for category >>>", category, keywords);
+
+  const newsResponse = await res.json();
+
+  const news = sortNewsByImage(newsResponse.data.myQuery);
+
+  return news;
+};
+
+export default fetchNews;
