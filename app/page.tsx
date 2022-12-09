e593e3c986c57efeab7c7284ccedace0af6f70c0
vberkoz <vberkoz@gmail.com>
2022-12-09 23:48:47 +0200

    Building the NewsList Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
+import response from "../responce.json";
 
 async function Homepage() {
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(",")));
 
   console.log(news);
 
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 }
 
 export default Homepage;
